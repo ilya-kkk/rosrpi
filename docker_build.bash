70915fb0e3f4e225d4d0dd96f4e0993e9e0a43c9
Ilya <121675360+ilya-kkk@users.noreply.github.com>
2024-11-12 17:53:38 +0300

    Update docker_build.bash

--- docker_build.bash
+++ docker_build.bash
@@ -2,9 +2,11 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 EXEC_PATH=$PWD
+echo "ROOT_DIR: $ROOT_DIR"
+echo "EXEC_PATH: $EXEC_PATH"
 
 cd $ROOT_DIR
-docker build -t ros_course2023-img -f $ROOT_DIR/docker/Dockerfile $ROOT_DIR \
+docker build -t ros_arm -f $ROOT_DIR/docker/Dockerfile $ROOT_DIR \
                                   --network=host \
                                   
 cd $EXEC_PATH
