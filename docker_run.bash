594db7254e9711f44be4e171af7bd153649b2364
Ilya <121675360+ilya-kkk@users.noreply.github.com>
2024-11-12 19:29:46 +0300

    Update docker_run.bash

--- docker_run.bash
+++ docker_run.bash
@@ -13,5 +13,5 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
                 -v $ROOT_DIR/workspace:/workspace \
                --net=host \
                --privileged \
-               --name ros_course2023 ros_course2023-img
+               --name ros_arm1 ros_arm
 
