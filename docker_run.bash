0261f8886bb118bfa54cc1ad749969f14343fcd4
Ilya <121675360+ilya-kkk@users.noreply.github.com>
2024-11-12 16:46:05 +0300

    1

---
+++ docker_run.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+xhost +local:docker || true
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+    docker run  -ti --rm \
+                -e "DISPLAY" \
+                -e "QT_X11_NO_MITSHM=1" \
+                -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+                -e XAUTHORITY \
+                -v /dev:/dev \
+                -v $ROOT_DIR/workspace:/workspace \
+               --net=host \
+               --privileged \
+               --name ros_course2023 ros_course2023-img
+
