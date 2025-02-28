3355fe917de7d17c6d28faae6dec3bcbf2e37460
ilya-kkk <i89213662231@gmail.com>
2025-02-28 22:34:15 +0300

    fix camera 1

--- rpi_run/rpi_docker_run.bash
+++ rpi_run/rpi_docker_run.bash
@@ -7,6 +7,9 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 docker pull ilya9kkk/ros_arm:latest
 
     docker run  -ti --rm \
+                --device /dev/vchiq \
+                --device /dev/video0 \
+                -v /opt/vc:/opt/vc \
                 -e "DISPLAY" \
                 -e "QT_X11_NO_MITSHM=1" \
                 -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
--- workspace/build/raspicam_node/catkin_generated/setup_cached.sh
+++ workspace/build/raspicam_node/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/workspace/devel/.private/raspicam_node:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/workspace/devel/.private/raspicam_node/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/workspace/devel/.private/raspicam_node/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/workspace/build/raspicam_node'
+export PYTHONPATH="/workspace/devel/.private/raspicam_node/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/workspace/devel/.private/raspicam_node/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/workspace/src/raspicam_node:$ROS_PACKAGE_PATH"
