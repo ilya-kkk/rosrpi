89c5eb23496274e4345a331d08e2cde8160eba4e
ilya-kkk <i89213662231@gmail.com>
2025-02-28 23:25:11 +0300

    kostyl fix 2

--- workspace/rpi_depend_KOSTYL.bash
+++ workspace/rpi_depend_KOSTYL.bash
@@ -1,27 +1,38 @@
 #!/bin/bash
 
 # Обновляем список пакетов
-sudo apt-get update
+echo "Обновляем список пакетов"
+sudo apt-get update > /dev/null 2>&1
 
 # Устанавливаем необходимые библиотеки для ARM64
-sudo apt-get install -y libc6-dev:arm64
+echo "Устанавливаем необходимые библиотеки для ARM64"
+sudo apt-get install -y libc6-dev:arm64 > /dev/null 2>&1
 
 # Устанавливаем библиотеку для работы с Raspberry Pi (включает MMAL)
-sudo apt-get install -y libraspberrypi-dev
+echo "Устанавливаем библиотеку для работы с Raspberry Pi (включает MMAL)"
+sudo apt-get install -y libraspberrypi-dev > /dev/null 2>&1
 
 # Устанавливаем библиотеку RT
-sudo apt-get install -y lib32rt1
+echo "Устанавливаем библиотеку RT"
+sudo apt-get install -y lib32rt1 > /dev/null 2>&1
 
 # Устанавливаем библиотеки Boost для C++
-sudo apt-get install -y libboost-all-dev
+echo "Устанавливаем библиотеки Boost для C++"
+sudo apt-get install -y libboost-all-dev > /dev/null 2>&1
 
 # Устанавливаем библиотеку Asio для работы с асинхронными операциями ввода/вывода
-sudo apt-get install -y libasio-dev
+echo "Устанавливаем библиотеку Asio для работы с асинхронными операциями ввода/вывода"
+sudo apt-get install -y libasio-dev > /dev/null 2>&1
 
-sudo apt install python3-rosdep
-sudo rosdep init
-rosdep update
-rosdep install --from-paths workspace/src --ignore-src -r -y
+echo "Устанавливаем python3-rosdep"
+sudo apt install python3-rosdep > /dev/null 2>&1
+echo "Инициализация rosdep"
+sudo rosdep init > /dev/null 2>&1
+echo "Обновление rosdep"
+rosdep update > /dev/null 2>&1
+echo "Установка зависимостей rosdep"
+rosdep install --from-paths workspace/src --ignore-src -r -y > /dev/null 2>&1
 
 # Выполнение скрипта container_start
+echo "Выполнение скрипта container_start"
 /workspace/container_start.bash
