45468040a5c88bfd907965df0594e0cbfa33c04a
ilya-kkk <i89213662231@gmail.com>
2025-02-28 17:50:04 +0300

    fix rpi dockerfile

---
+++ docker_dev/rpi_docker_build.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+EXEC_PATH=$PWD
+echo "ROOT_DIR: $ROOT_DIR"
+echo "EXEC_PATH: $EXEC_PATH"
+
+cd $ROOT_DIR
+docker build -t ros_arm -f $ROOT_DIR/rosrpi/rpi_Dockerfile $ROOT_DIR \
+                                  --network=host \
+                                  
+cd $EXEC_PATH
