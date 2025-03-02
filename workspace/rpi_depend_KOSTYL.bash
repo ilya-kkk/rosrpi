39f579060c7b979189f614193c133f2ab6a035a1
ilya-kkk <i89213662231@gmail.com>
2025-03-02 17:10:40 +0300

    fix camera 5

--- workspace/rpi_depend_KOSTYL.bash
+++ workspace/rpi_depend_KOSTYL.bash
@@ -15,6 +15,29 @@ sudo apt-get install -y libboost-all-dev
 # Устанавливаем библиотеку Asio для работы с асинхронными операциями ввода/вывода
 sudo apt-get install -y libasio-dev
 
+apt update && apt install -y v4l-utils  
+apt update
+
+######## СМЕРТЕЛЬНЫЙ НОМЕР, СОБИРАЕМ ПАКЕТ ИЗ ИСХОДНИКОВ ################
+apt update && apt install -y \
+    cmake ninja-build pkg-config python3-pip \
+    python3-yaml python3-ply python3-jinja2 python3-pyparsing \
+    python3-dev python3-pil python3-scipy \
+    libboost-dev libgnutls28-dev openssl \
+    libtiff-dev libjpeg-dev libpng-dev \
+    libevent-dev libcurl4-openssl-dev \
+    libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+
+git clone --depth=1 https://git.libcamera.org/libcamera/libcamera.git
+cd libcamera
+
+mkdir build && cd build
+cmake -GNinja ..
+ninja
+ninja install
+ldconfig
+########################################################################
+
 sudo apt install python3-rosdep
 sudo rosdep init
 rosdep update
