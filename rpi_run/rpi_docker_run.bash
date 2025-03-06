88cace32b8647ff6865654550a0393f210c89560
ilya-kkk <i89213662231@gmail.com>
2025-03-07 01:43:42 +0300

    make launchfile for rpi

--- rpi_run/rpi_docker_run.bash
+++ rpi_run/rpi_docker_run.bash
@@ -19,5 +19,5 @@ docker run -ti --rm \
     --net=host \
     --ipc=host \
     --privileged \
-    --name ros_rpi ilya9kkk/ros_arm:latest \
+    --name ros_rpi ilya9kkk/ros_arm:camera \
 # bash -c "sleep 5 && /workspace/rpi_depend_KOSTYL.bash && exec bash"
