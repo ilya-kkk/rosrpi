ad5bef14a3e5d2256ad00fb96db870b080a46cb5
ilya-kkk <i89213662231@gmail.com>
2025-03-21 01:10:11 +0300

    8

--- workspace/CONTAINER_START.bash
+++ workspace/CONTAINER_START.bash
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 set -e  # Останавливает выполнение скрипта при ошибке
 
-echo "CONTAINER START SCRIPT RUNNING..."
+echo "${YELLOW}CONTAINER START SCRIPT RUNNING...${NC}"
 
 # Открываем интерактивную оболочку Bash и выполняем команды внутри неё
 /bin/bash -i << EOF
 
-echo "Переходим в директорию workspace..."
+echo "${YELLOW}Переходим в директорию workspace...${NC}"
 cd workspace
 
-echo "Строим catkin workspace..."
+catkin clean
+echo "${YELLOW}Строим catkin workspace...${NC}"
 catkin build
 
-echo "Настройка окружения с помощью setup.bash..."
+echo "${YELLOW}Настройка окружения с помощью setup.bash...${NC}"
 source devel/setup.bash
 
-EOF
