f28af200b9383c011e974d14546e2c18ba401940
ilya-kkk <i89213662231@gmail.com>
2025-03-21 15:07:35 +0300

    5

--- workspace/CONTAINER_START.bash
+++ workspace/CONTAINER_START.bash
@@ -23,6 +23,7 @@ catkin build
 echo "${YELLOW}Настройка окружения с помощью setup.bash...${NC}"
 source devel/setup.bash
 
+roslaunch core start.launch
 EOF
 
 # Оставляем терминал открытым после выполнения скрипта
