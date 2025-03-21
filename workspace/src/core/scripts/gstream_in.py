a1a1740b43ae5ef78cccf62f72ddfb06d539dbd1
ilya-kkk <i89213662231@gmail.com>
2025-03-22 01:29:19 +0300

    1

--- workspace/src/core/scripts/gstream_in.py
+++ workspace/src/core/scripts/gstream_in.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import subprocess
 import socket
 
 class GStreamerVideoReceiver:
