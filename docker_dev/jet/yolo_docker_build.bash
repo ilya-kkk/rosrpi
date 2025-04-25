991acb06c61aa16ef0476f76e243f91e256ccf4e
ilya-kkk <i89213662231@gmail.com>
2025-04-25 17:09:52 +0300

    add dockerfile for yolo

---
+++ docker_dev/jet/yolo_docker_build.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+EXEC_PATH=$PWD
+echo "ROOT_DIR: $ROOT_DIR"
+echo "EXEC_PATH: $EXEC_PATH"
+
+cd $ROOT_DIR
+docker build --platform linux/arm64 -t jet -f $ROOT_DIR/jet/Dockerfile_yolo $ROOT_DIR \
+                                  --network=host \
+                                  
+cd $EXEC_PATH
