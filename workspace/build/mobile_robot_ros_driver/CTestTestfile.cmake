257c1b5d85cc0f50266f58eef4b48f14684e1017
Leonid-Zykin <leonidzykin263@gmail.com>
2025-06-13 12:06:01 +0300

    All trash added to gitignore

--- workspace/build/mobile_robot_ros_driver/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/src/mobile_robot_ros_driver
-# Build directory: /workspace/build/mobile_robot_ros_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- workspace/build/raspicam_node/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/src/raspicam_node
-# Build directory: /workspace/build/raspicam_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
